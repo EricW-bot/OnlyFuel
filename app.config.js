c9ecb45360618d3b4afe243c30763421ba6cad0f
Eric Wong <z5692161@ad.unsw.edu.au>
2026-06-09 17:49:46 +1000

    UPDATE: increment ver

--- app.config.js
+++ app.config.js
@@ -19,7 +19,7 @@ module.exports = () => {
     name: 'OnlyFuel',
     slug: 'FuelNearMe',
     scheme: 'fuelnearme',
-    version: '2.2.0',
+    version: '2.3.0',
     orientation: 'portrait',
     icon: './assets/logo.png',
     userInterfaceStyle: 'automatic',
